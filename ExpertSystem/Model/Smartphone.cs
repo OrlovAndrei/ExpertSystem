2f2f73e964b559599e17f5fdf0aebb2c30ec4267
Andrey <androrvit@gmail.com>
2023-01-19 14:35:52 +0300

    add attributes to properties

--- ExpertSystem/Model/Smartphone.cs
+++ ExpertSystem/Model/Smartphone.cs
@@ -9,30 +9,39 @@ namespace ExpertSystem.Model
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
+        [Label("Компания:", "")]
         [JsonProperty(PropertyName = "company")]
         public string Company { get; set; }
 
+        [Label("Цена:", "$")]
         [JsonProperty(PropertyName = "price")]
         public int Price { get; set; }
 
+        [Label("Сеть:", "")]
         [JsonProperty(PropertyName = "network")]
         public string Network { get; set; }
 
+        [Label("OS:", "")]
         [JsonProperty(PropertyName = "os")]
         public string OS { get; set; }
 
+        [Label("Оперативная память:", "GB")]
         [JsonProperty(PropertyName = "ram")]
         public int RAM { get; set; }
 
+        [Label("Память:", "GB")]
         [JsonProperty(PropertyName = "storage")]
         public int Storage { get; set; }
 
+        [Label("Разрешение экрана:", "")]
         [JsonProperty(PropertyName = "resolution")]
         public string Resolution { get; set; }
 
+        [Label("Размер:", "\"")]
         [JsonProperty(PropertyName = "creationDate")]
         public double Size { get; set; }
 
+        [Label("Камера:", "")]
         [JsonProperty(PropertyName = "isCamera")]
         public bool IsCamera { get; set; }
 
